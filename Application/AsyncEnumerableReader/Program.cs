80c26cf37d19d8b0a02cb31b347acae567482393
ChjenNV <ChjenNV@elcomplus.ru>
2022-04-18 13:58:39 +0700

    Added AsyncEnumerable project

---
+++ Application/AsyncEnumerableReader/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using WordsReader;
+
+
+WordsCollection wordsCollection = new WordsCollection(Directory.GetCurrentDirectory() + "\\text.txt");
+await foreach (var word in wordsCollection.GetWords())
+{
+    //тут какая-то логика по валидации, проверки, итд
+    Console.WriteLine(word);
+}
+
+Console.ReadLine();
---
+++ Application/AsyncEnumerableReader/WordsCollection.cs
@@ -0,0 +1,29 @@
+﻿namespace WordsReader
+{
+    public class WordsCollection
+    {
+        private readonly StreamReader _streamReader;
+
+        public WordsCollection(string filePath)
+        {
+            if (filePath == null)
+                throw new ArgumentNullException(nameof(filePath));
+            _streamReader = new StreamReader(filePath);
+        }
+
+        public async IAsyncEnumerable<string> GetWords()
+        {
+            while (true)
+            {
+                var line = await _streamReader.ReadLineAsync();
+                if (line == null)
+                    yield break;
+                var words = line.Split(' ', StringSplitOptions.None);
+                foreach (var word in words)
+                {
+                    yield return word;
+                }
+            }
+        }
+    }
+}
